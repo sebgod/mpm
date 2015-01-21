cc812c79300905564e027cb746709907f532d672
Sebastian Godelet <sebastian.godelet@outlook.com>
2015-01-21 21:20:35 +0800

    Bootstrapping of manual mpm is working.
    src/Makefile:
        contains prototype for integrated building of mercury libraries.
    
    src/cli:
        this will be the mpm command line module (as in npm).
    
    src/mpm:
        the exectuable mpm forwarding all I/O to cli.
    
    src/semver:
        implementation of semantic versioning.

---
+++ src/mercury_mpm.m
@@ -0,0 +1,23 @@
+%----------------------------------------------------------------------------%
+% vim: ft=mercury ff=unix ts=4 sw=4 et
+%----------------------------------------------------------------------------%
+% File: mercury_mpm.m
+% Copyright © 2015 Sebastian Godelet
+% Main author: Sebastian Godelet <sebastian.godelet@outlook.com>
+% Created on: Wed Jan 21 12:25:26 CST 2015
+% Stability: low
+%----------------------------------------------------------------------------%
+% Library interface for `mercury_mpm'.
+%----------------------------------------------------------------------------%
+
+:- module mercury_mpm.
+
+:- interface.
+
+:- include_module mercury_mpm.cli.
+:- include_module mercury_mpm.meta_info.
+
+%----------------------------------------------------------------------------%
+
+:- end_module mercury_mpm.
+%----------------------------------------------------------------------------%
